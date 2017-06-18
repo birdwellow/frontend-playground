2ac5e8cd686594d55a21b8f3612df4752b1213d3
Florian Vogel <florian.vogel@iteratec.de>
2017-06-18 21:36:45 +0200

    Implement an interactive map with D3

--- try-d3/src/index.js
+++ try-d3/src/index.js
@@ -1,41 +1,61 @@
 var d3 = require('d3');
+var shapeData = require('./shapeData.json');
 
 function start() {
     var svg = d3.select('svg')
         .attr('width', 960)
         .attr('height', 500);
 
-    var g = svg.append('g');
-    g.selectAll('circle')
-        .data([32,8,26,12,9,2,34])
-        .enter().append('circle')
-        .attr('transform', function() {
-            return 'translate(' + Math.random() * 100 + 200 + ',' + Math.random() * 50 + ')';
+    var g = svg.selectAll("g")
+        .data(shapeData)
+        .enter()
+        .append("g")
+        .attr("class", "groups");
+
+    g.append("path")
+        .attr("d", function (country) {
+            return country.svgData;
+        });
+    g.append("text")
+        .text(function (country) {
+            return country.name;
         })
-        .style('fill', function() {
-            return 'hsl(' + Math.random() * 360 + ',100%,50%)';
+        .attr("x", function (d) {
+            return d.center.x;
         })
-        .attr('r', function (d) {
-            return d;
+        .attr("y", function (d) {
+            return d.center.y;
         });
 
-        // .selectAll('#drawing-area')
-        // .data([4, 8, 15, 16, 23, 42])
-        // .enter().append('div')
-        //
-        // .style('background', function() {
-        //     return 'hsl(' + Math.random() * 360 + ',100%,50%)';
-        // })
-        // .style('width', function (d) {
-        //     return d + 'px';
-        // })
-        // .style('height', function (d) {
-        //     return d + 'px';
-        // })
-        // .style('position', 'absolute')
-        // .style('margin', function (d) {
-        //     return d + 'px 0 0 ' + d + 'px';
-        // });
+    g.on('click', function (data) {
+        data.selected = !data.selected;
+        update();
+    });
+
+    var update = function () {
+        g.data(shapeData);
+
+        g.attr('class', function (country) {
+            return country.selected ? 'groups selected' : 'groups';
+        });
+
+        g.selectAll("path")
+            .attr("d", function (country) {
+                return country.svgData;
+            });
+        g.selectAll("text")
+            .text(function (country) {
+                return country.name;
+            })
+            .attr("x", function (d) {
+                return d.center.x;
+            })
+            .attr("y", function (d) {
+                return d.center.y;
+            });
+    };
+
+    //svg.attr("transform", "scale(2)");
 }
 
 start();
